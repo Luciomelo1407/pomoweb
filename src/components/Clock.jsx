b6e420e4acfc64ac9da1acdb24cefcc245624efb
maria eugenia <luciomelo90@gmail.com>
2024-12-11 09:41:13 -0300

    mais um commit

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -2,20 +2,26 @@
 import { useState } from "react";
 
 function Clock(){
+  const [working,setWorking] = useState(true);
+
   const buttonStart = <button onClick={start} className="bg-[#D9D9D9] p-2 rounded-sm border-b-4 border-[#F9F6F6]" >Start</button>
   const buttonContinue = <div><button onClick={start} className="bg-[#D9D9D9] p-2 rounded-sm border-b-4 border-[#F9F6F6] mr-10">Continue</button> <button className="bg-[#D9D9D9] p-2 rounded-sm border-b-4 border-[#F9F6F6]">Skip</button></div>
-  const buttonPause = <div><button onClick={pause} className="bg-[#D9D9D9] p-2 rounded-sm border-b-4 border-[#F9F6F6] mr-10">Pause</button> <button className="bg-[#D9D9D9] p-2 rounded-sm border-b-4 border-[#F9F6F6]">Skip</button></div>
-  
-  const [minutos, setMinutos] = useState("01")
-  const [segundos, setSegundos] = useState("10")
-  const [control, setControl] = useState (buttonStart)
-  let varSegundos = parseInt(segundos);
-  let varMinutos = parseInt(minutos);
-  let state = 'stop';
-  let working = true;
+  const buttonPause = <div><button onClick={pause} className="bg-[#D9D9D9] p-2 rounded-sm border-b-4 border-[#F9F6F6] mr-10">Pause</button> <button onClick={skip} className="bg-[#D9D9D9] p-2 rounded-sm border-b-4 border-[#F9F6F6]">Skip</button></div>
 
-  const restTime = 0;
-  const workTime = 0;
+  let restTimeSeconds = 0;
+  let restTimeMinutes = 1;
+  let workTimeSeconds = 3;
+  let workTimeMinutes = 2;
+
+  const [minutos, setMinutos] = useState(workTimeMinutes.toString())
+  const [segundos, setSegundos] = useState(workTimeSeconds.toString())
+  const [control, setControl] = useState (buttonStart)
+  let varSegundos = workTimeSeconds;
+  let varMinutos = workTimeMinutes;
+  let state = 'stop';
+  
+
+
 
   let setInterface = () => {
     console.log("teste")
@@ -32,7 +38,7 @@ function Clock(){
   }
 
   function clock(){
-    varSegundos = varSegundos - 1
+  varSegundos = varSegundos - 1
     if(varSegundos == -1 && varMinutos != 0){
       varMinutos = varMinutos -1
     }
@@ -43,12 +49,25 @@ function Clock(){
     if(state == 'running'){
       updateTimer();
       if(varMinutos == 0 && varSegundos==0){
-        state = 'finished'
+        state = 'paused'
+        console.log(working)
+        setInterface()
+        if (working) {
+          varSegundos = restTimeSeconds
+          varMinutos = restTimeMinutes
+        }else{
+          varSegundos = workTimeSeconds;
+          varMinutos = workTimeMinutes;
+        }
+        setWorking(!working)
+        updateTimer();
+        
+
         /*
           Bloco de código para configuração para o próximo intervalo de tempo
         */ 
       }
-      setTimeout(clock,1000)
+      setTimeout(clock,100)
     }
   }
   
@@ -89,10 +108,10 @@ function Clock(){
 
     return(
         <>
+                {working ? <p className="text-center">Working ...</p> : <p className="text-center">Resting ...</p>}
                 <div className="font-mono text-9xl m-auto mt-32">
                     <span className="bg-[#D9D9D9] rounded-lg shadow-lg">{minutos}</span> <span>:</span> <span className="bg-[#D9D9D9] rounded-lg shadow-lg">{segundos}</span>
                 </div>
-
             <div className="mx-auto mt-20 mb-24">{control}</div>
         </>
     )
